581bf1e4d6c08a8bd9658427c99f58f14f0884b1
tahir <tahir.farooq@altia.es>
2022-02-07 17:43:43 +0100

    adding my action in create-react-app11

--- scaffolder-templates/create-react-app/skeleton/src/App.tsx
+++ scaffolder-templates/create-react-app/skeleton/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ${{parameters.contents}}
         </a>
       </header>
     </div>
